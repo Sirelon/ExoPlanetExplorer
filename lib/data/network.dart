2b0be2d70f89359d6dfc6dc4a45d05c4af093cca
Aleksandr Romanishyn <aleksandr.romanishyn@appflame.com>
2021-01-03 23:07:36 +0200

    Prepare loading by query.

--- lib/data/network.dart
+++ lib/data/network.dart
@@ -7,13 +7,46 @@ const String HOST = "https://exoplanets.nasa.gov";
 
 const String _API = HOST + "/api/v1/";
 
-Future<ListResponse> loadList() async {
-  final response = await http.get(
-      _API + "planets/" + "?order=display_name+asc&per_page=10&page=0&search=");
+Future<ListResponse> loadList(Query query) async {
+  final response = await http.get(_API +
+      "planets/" +
+      "?order=${query._order}&per_page=${query._perPage}&search=${query._search}&page=${query._page}");
+
   if (response.statusCode == 200) {
     final json = jsonDecode(response.body);
+
+    var e = ListResponse.fromMap(json).items;
+
+    // var s = e.map((e) => "'$HOST" + e.url + "'").toString();
+    // print(s);
+    //
+
     return ListResponse.fromMap(json);
   } else {
     return Future.error("Http error ${response.reasonPhrase}");
   }
+}
+
+class Query {
+  var _order = "display_name+asc";
+  var _perPage = 10;
+  var _page = 0;
+  var _search = "";
+
+  nextPage() {
+    _page++;
+  }
+
+  search(String search) => _search = search;
+
+  void nextPageFromSize(int size) {
+    _page = size ~/ _perPage;
+    print("Page is $_page");
+  }
+}
+
+extension UI on ExoPlanet {
+  String imageUrl() => HOST + image;
+
+  String listImageUrl() => HOST + listImage;
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:exo_planets/data/network.dart';
 import 'package:exo_planets/data/response.dart';
 import 'package:flutter/material.dart';
@@ -14,19 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
@@ -37,15 +26,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -53,18 +33,12 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final _query = Query();
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: _pageWidget(),
@@ -74,16 +48,34 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget _pageWidget() {
     return PaginationView(
         itemBuilder: (BuildContext context, ExoPlanet planet, int index) =>
-            ListTile(
-              title: Text(planet.title),
-              subtitle: Text(planet.description),
-              leading: IconButton(
-                icon: Icon(Icons.person),
-                onPressed: () => null,
-              ),
-            ),
-        pageFetch: (size) => loadList().then((value) => value.items),
+            PlanetItemWidget(planet),
+        pageFetch: (size) {
+          _query.nextPageFromSize(size);
+          return loadList(_query).then((value) => value.items);
+        },
         onEmpty: null,
         onError: null);
+  }
+}
+
+class PlanetItemWidget extends StatelessWidget {
+  ExoPlanet planet;
+
+  PlanetItemWidget(this.planet, {
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // return CachedNetworkImage(imageUrl: planet.listImageUrl());
+
+    return ListTile(
+      title: Text(planet.title),
+      subtitle: Text(planet.description),
+      leading: IconButton(
+        icon: Icon(Icons.person),
+        onPressed: () => null,
+      ),
+    );
   }
 }
